e73856e91e6dc22451dc1df0e4868d2c64bbde08
Daniel Carral <dan@dcarral.org>
2015-10-04 23:56:29 +0200

    Add example in Rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
-#[test]
-fn it_works() {
+use std::thread;
+
+fn process() {
+    let handles: Vec<_> = (0..10).map(|_| {
+        thread::spawn(|| {
+            let mut x = 0;
+            for _ in (0..5_000_000) {
+                x += 1
+            }
+            x
+        })
+    }).collect();
+
+    for handle in handles {
+        println!("Thread finished with count={}", handle.join().map_err(|_| "Could not join a thread!").unwrap());
+    }
+
+    println!("done!");
 }
